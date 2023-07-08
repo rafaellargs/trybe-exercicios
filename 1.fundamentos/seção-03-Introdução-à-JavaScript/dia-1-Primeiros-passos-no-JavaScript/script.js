5b8273e19d02ee8addeb26dd5902724c695c2d3e
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-08 17:30:39 -0300

    Resposta dos Exercícios 2 e 3

--- 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
@@ -9,3 +9,12 @@ console.log('Subtração: ' + (num1 - num2));
 console.log('Multiplicação: ' + (num1 * num2));
 console.log('Divisão: ' + (num1 / num2));
 console.log('Módulo: ' + (num1 % num2));
+
+// Exercício 2
+
+let numero1 = 4;
+let numero2 = 45;
+
+if (numero1 > numero2) {
+    console.log(numero1 + ' é maior que ' + numero2);
+}
