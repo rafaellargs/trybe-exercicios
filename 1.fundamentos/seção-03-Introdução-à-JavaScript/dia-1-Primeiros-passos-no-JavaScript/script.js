17324560b50bc2f5e8654df917391fd4d57d3554
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-08 17:54:12 -0300

    Resposta dos Exercícios 4

--- 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
@@ -1,5 +1,12 @@
 // Introdução à JavaScript - 3.1.Primeiros Passos
 // Exercício 1
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+// Adição (num1 + num2)
+// Subtração (num1 - num2)
+// Multiplicação (num1 * num2)
+// Divisão (num1 / num2)
+// Módulo (num1 % num2)
 
 let num1 = 20;
 let num2 = 5;
@@ -11,10 +18,69 @@ console.log('Divisão: ' + (num1 / num2));
 console.log('Módulo: ' + (num1 % num2));
 
 // Exercício 2
+// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
 
-let numero1 = 4;
-let numero2 = 45;
+let numero1 = 6;
+let numero2 = 4;
 
 if (numero1 > numero2) {
     console.log(numero1 + ' é maior que ' + numero2);
+} else if (numero1 === numero2) {
+    console.log('Os números são iguais!');
+} else {
+    console.log(numero1 + ' é menor que ' + numero2);
 }
+
+//Exercício 3 
+// Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+let number1 = 4;
+let number2 = 35;
+let number3 = 55;
+
+if (number1 > number2 && number1 > number3){
+    console.log(number1 + ' é o maior número.');
+} else if (number2 > number1 && number2 > number3) {
+    console.log(number2 + ' é o maior número.');
+} else if (number3 > number1 && number3 > number2){
+    console.log(number3 + ' é o maior número.');
+} else {
+    console.log('Existem números iguais!');
+}
+
+// 🚀 Exercício 4
+// Utilize if...else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+// 👀 Dica: para os ângulos serem de um triângulo válido, a soma dos três ângulos deve ser 180 graus. Um ângulo será considerado inválido se não tiver um valor positivo.
+
+let anguloA = 40;
+let anguloB = 20;
+let anguloC = 120;
+
+let somaAngulos = anguloA + anguloB + anguloC;
+let angulospositivos = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+if(angulospositivos){
+    if (somaAngulos === 180){
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+} else {
+    console.log('Erro: ângulo inválido!');
+}
+
+// Exercício 5
+// Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+
+// Atenção ⚠️: não sabe o que é switch/case? Reveja a aula pois pode ter passado despercebido ☺️. 
+// Caso queira se aventurar na aprendizagem do switch/case através da documentação, tem um link muito bom te esperando na lição de Recursos Adicionais. 
+// Por fim, você terá mais oportunidades de sedimentar o conhecimento sobre a estrutura condicional switch/case ao longo da formação! Agora, só queremos dar visibilidade que existem outras ferramentas para fazer operações condicionais, ok?
+
+// Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+// ⭐️ Desafio extra, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais. 
+// Uma dica é pesquisar uma função que faça uma string ficar com todas as letras minúsculas (lower case).
+
+// Exemplo: Bispo -> Diagonais.
+
+    
+
