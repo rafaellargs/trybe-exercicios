0812e7dc2948a1cfda9a4ff6c0a6af32c6ca304b
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-08 16:17:14 -0300

    Cria pasta seção 03 - Introdução à Javascript e resposta do Exercício 1

---
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
@@ -0,0 +1,11 @@
+// Introdução à JavaScript - 3.1.Primeiros Passos
+// Exercício 1
+
+let num1 = 20;
+let num2 = 5;
+
+console.log('Adição: ' + (num1 + num2));
+console.log('Subtração: ' + (num1 - num2));
+console.log('Multiplicação: ' + (num1 * num2));
+console.log('Divisão: ' + (num1 / num2));
+console.log('Módulo: ' + (num1 % num2));
