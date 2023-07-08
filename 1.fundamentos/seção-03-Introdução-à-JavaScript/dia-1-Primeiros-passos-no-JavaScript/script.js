c55b4157bccec07e12706f9923a774e5cd732d18
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-08 18:02:10 -0300

    Resposta do Exercício 5

--- 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-1-Primeiros-passos-no-JavaScript/script.js
@@ -82,5 +82,28 @@ if(angulospositivos){
 
 // Exemplo: Bispo -> Diagonais.
 
-    
+let xadrez = 'Rei';
+
+switch  (xadrez.toLowerCase()) {
+  case 'rei':
+    console.log('Rei -> Uma casa para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo -> Diagonais.');
+    break;
+  case 'rainha':
+    console.log('Rainha -> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+}   
 
