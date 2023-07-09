86c722ef6b0601a3c6451b00d5f51affc11ae8d1
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-09 10:29:58 -0300

    Cria pasta do dia 2 - Array e estrutura de repetição e exercícios de fixação

---
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-2-Array-e-estrutura-de-repetição/script.js
@@ -0,0 +1,19 @@
+//Arrays
+//Exercício 1
+// Altere o valor da variável menuServices para que ela passe a ter o valor “Serviços”. Isso deve ser feito através da variável menu.
+
+// Exercício 2
+// Adicione o valor “Contato” no final do array menu.
+
+const menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+const menuServices = menu[1];
+
+console.log(menuServices);
+
+menu.push('Contato');
+
+console.log(menu);
+
+
+
+
