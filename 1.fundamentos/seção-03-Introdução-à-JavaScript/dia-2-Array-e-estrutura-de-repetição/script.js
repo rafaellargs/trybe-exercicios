cba7f96885c606195f5f5c3b012406d309e238fd
Rafaella Reis <rafaellareisgs@gmail.com>
2023-07-09 12:30:46 -0300

    Resposta do exercício de fixação sobre Estruturas de Repetição for

--- 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-2-Array-e-estrutura-de-repetição/script.js
+++ 1.fundamentos/seção-03-Introdução-à-JavaScript/dia-2-Array-e-estrutura-de-repetição/script.js
@@ -14,6 +14,18 @@ menu.push('Contato');
 
 console.log(menu);
 
+// Estrutura de repetição for
+// Para fixar
+// Chegou o momento de testar os conhecimentos apresentados no conteúdo do dia. Vamos nessa?
 
+// Exercício 1
+// Utilize o for para imprimir os elementos da lista groceryList através de console.log():
 
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
 
+for(let index = 0; index < groceryList.length; index += 1) {
+    // const currentItem = groceryList[index];
+    // console.log(currentItem);
+    console.log(`${index} - ${groceryList[index]}`);
+}
+  
